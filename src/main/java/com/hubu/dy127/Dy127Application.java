cb7e01b6f08042482053753608c3fe2a76ffc84f
admin <2678405901@qq.com>
2021-10-28 22:13:35 +0800

    Initial commit

---
+++ src/main/java/com/hubu/dy127/Dy127Application.java
@@ -0,0 +1,13 @@
+package com.hubu.dy127;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dy127Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dy127Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hubu/dy127/Dy127ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hubu.dy127;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dy127ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
